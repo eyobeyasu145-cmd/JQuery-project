6a4853abf43f593fb04c7442a3f32024816cbc71
Eyob <eyobeyasu145@gmail.com>
2025-11-05 09:03:14 +0300

    now I add css properts in my jquery

--- JQ.js
+++ JQ.js
@@ -1,3 +1,6 @@
 function func1(){
-  $("#img1").fadeToggle(3000);
+  // $("div").css('background-color' , 'orange');
+  $(".p1").css('font-style','italic');
+  $("#div1 , #div2 , li").css('background-color' ,'purple')
 }
+  $("#div1").css('shadow-box' , '');
