b96230999ff8043e18a96f5deee2ed98f15268b2
Eyob <eyobeyasu145@gmail.com>
2025-11-05 13:45:38 +0300

    now I add select and how to hide text also use eq() select which tag  properts in my jquery

--- JQ.js
+++ JQ.js
@@ -1,6 +1,16 @@
 function func1(){
   // $("div").css('background-color' , 'orange');
   $(".p1").css('font-style','italic');
-  $("#div1 , #div2 , li").css('background-color' ,'purple')
+    // $("#div3 li").text("all list is similar now");
+    $("#div3 ul").append("<li> New list<li>");
+    $("#div3 ul").prepend("<li>the first list item<li>");
+    $("#div3 ul").after("<li>after selector<li>");
+    $("#div3 ul").before("<li>what means before selector<li>");
+    
+
+  //  $("div > p").fadeToggle();
+    //  $("p:first").fadeToggle();
 }
-  $("#div1").css('shadow-box' , '');
+for (let i =0 ; i<3 ; i++){
+       $("p:eq(i)").hide();
+}
