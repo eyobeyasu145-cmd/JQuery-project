c6ddea97e39072bca875fc5106028e4d9fb5368a
Eyob <eyobeyasu145@gmail.com>
2025-11-07 08:29:24 +0300

    session of animate in JQuery

---
+++ animate.js
@@ -0,0 +1,10 @@
+
+ $("document").ready(function(){
+  $("#btn").click(function(){
+   $("#img").animate({
+      left:'150px',
+      opacity:'1',
+      width:'400px', 
+   height:'400px'  }, '2000') // 2000 mil second
+  })
+  });
